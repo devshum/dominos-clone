32f3b3c83d23175f5d11a894b7d3b88715adcafc
devshum <57689318+devshum@users.noreply.github.com>
2022-06-10 14:36:46 +0300

    routing module was set up + minor preparation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  // { 
+  //   path: '',
+  //   pathMatch: 'full',
+  //   redirectTo: ''
+  // },
+  { path: '', loadChildren: () => import('./module-pizzas/pizzas.module').then(m => m.PizzasModule)},
+  { path: 'drinks', loadChildren: () => import('./module-drinks/drinks.module').then(m => m.DrinksModule)},
+  // { path: '**', redirectTo: ''}
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
