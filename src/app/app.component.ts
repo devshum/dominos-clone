2efa488c83a7b87df55b6025e7f386deb3ae4597
devshum <57689318+devshum@users.noreply.github.com>
2022-06-12 17:58:50 +0300

    auth service was updated (autologin, logout)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { AuthService } from 'src/app/core/services/auth/auth.service';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { takeUntil } from 'rxjs/operators';
+import { Subject } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'dominos';
+export class AppComponent implements OnInit, OnDestroy {
+  private _unsubscribe: Subject<any> = new Subject<any>();
+
+  constructor(
+    private _authService: AuthService
+  ) {}
+
+  ngOnInit(): void {
+    this._authService.autoLogin().pipe(
+      takeUntil(this._unsubscribe)
+    ).subscribe();
+  }
+
+  ngOnDestroy(): void {
+    this._unsubscribe.next();
+  }
 }
