0da7d70707a0fac5d6b1b6ed98d136e9964a7bae
devshum <57689318+devshum@users.noreply.github.com>
2022-06-12 14:33:06 +0300

    iterceptor was added

---
+++ src/app/core/interceptors/auth-interceptor.interceptor.ts
@@ -0,0 +1,35 @@
+import { AuthService } from './../services/auth.service';
+import { Injectable } from '@angular/core';
+import {
+  HttpRequest,
+  HttpHandler,
+  HttpInterceptor,
+  HttpEvent
+} from '@angular/common/http';
+import { concatMap, filter, take } from 'rxjs/operators';
+import { Observable } from 'rxjs';
+import { LoginResponse } from '../interfaces/login-response';
+
+@Injectable()
+export class AuthInterceptor implements HttpInterceptor {
+
+  constructor(
+    private _authService: AuthService
+  ) {}
+
+  intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    return this._authService.credentials$.pipe(
+      take(1),
+      concatMap((credentials: LoginResponse | null) => {
+        if(credentials?.token) {
+           request = request.clone({
+            setHeaders: {
+              authorization: `Bearer ${credentials.token}`,
+            }
+          })
+        }
+        return next.handle(request);      
+      })
+    )
+  }
+}
--- src/app/module-layout/layout.module.ts
+++ src/app/module-layout/layout.module.ts
@@ -6,6 +6,8 @@ import { LayoutComponent } from './components/layout/layout.component';
 import { AuthComponent } from './components/auth/auth.component';
 import { AppRoutingModule } from '../app-routing.module';
 import { ReactiveFormsModule } from '@angular/forms';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { AuthInterceptor } from '../core/interceptors/auth-interceptor.interceptor';
 
 
 
@@ -20,6 +22,13 @@ import { ReactiveFormsModule } from '@angular/forms';
     CommonModule,
     AppRoutingModule,
     ReactiveFormsModule
+  ],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: AuthInterceptor,
+      multi: true
+    }
   ],
   exports: [
     LayoutComponent
